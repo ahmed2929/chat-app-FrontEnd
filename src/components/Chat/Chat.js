51b145016d067a7dd8f3f8dc9045ffc7a3a5d42f
ahmed khaled <34711604+ahmed2929@users.noreply.github.com>
2021-10-06 11:53:33 +0200

    Update Chat.js
    update socket endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://chat-ak-app-backend.herokuapp.com/';
+  const ENDPOINT = 'https://chat2929.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
